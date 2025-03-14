f20f422d9d9810c3655bfe0fd261961da56291fb
Islam <islamcobra00@gmail.com>
2025-03-14 03:35:02 +0200

    04-EF

---
+++ 04-EF/Entities/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_EF.Entities
+{
+    public abstract class Vehicle
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+    }
+    public  class Car : Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+        public override string ToString()
+        {
+            return $"Car = Id = {Id} , Make = {Make} , Model = {Model} , NumberOfDoors = {NumberOfDoors}";
+        }
+    }
+    public class Truck : Car
+    {
+        public int LoadCapacity { get; set; }
+    }
+
+}
---
+++ 04-EF/Migrations/20250313234549_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _04_EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Make = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VehicleType = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    NumberOfDoors = table.Column<int>(type: "int", nullable: true),
+                    LoadCapacity = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
